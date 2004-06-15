59eb9ee9b9e418e0c254fd019b4665a5dee5f405
Marco d'Itri <md@linux.it>
2004-06-15 12:39:03 +0200

    Debian release 1:1.7.5-1

--- debian/patches/misc_autoconf.h
+++ debian/patches/misc_autoconf.h
@@ -1,11 +1,15 @@
 --- tin-1.7.3+20031209.orig/include/autoconf.hin
 +++ tin-1.7.3+20031209/include/autoconf.hin
-@@ -846,4 +846,8 @@
+@@ -846,4 +846,12 @@
   */
  #	undef XFACE_ABLE
  
 +/* debian-specific */
 +#define XFACE_ABLE
 +#define NNTP_SERVER_FILE "/etc/news/server"
++#undef  HAVE_METAMAIL
++#define HAVE_METAMAIL 1
++#undef  PATH_METAMAIL
++#define PATH_METAMAIL "metamail"
 +
  #endif /* !TIN_AUTOCONF_H */
