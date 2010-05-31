928830ffd1ce3b6b76bbaf1b6aa76229497112d8
Marco d'Itri <md@linux.it>
2010-06-01 00:29:38 +0200

    Debian release 1:1.9.6~20100522-1

--- debian/patches/misc_autoconf.h
+++ debian/patches/misc_autoconf.h
@@ -1,15 +1,11 @@
---- tin-1.7.3+20031209.orig/include/autoconf.hin
-+++ tin-1.7.3+20031209/include/autoconf.hin
-@@ -846,4 +846,12 @@
+--- a/include/autoconf.hin
++++ b/include/autoconf.hin
+@@ -892,4 +892,8 @@ Added missing headers after gettext upda
   */
- #	undef XFACE_ABLE
+ #	undef DISABLE_PIPELINING
  
 +/* debian-specific */
 +#define XFACE_ABLE
 +#define NNTP_SERVER_FILE "/etc/news/server"
-+#undef  HAVE_METAMAIL
-+#define HAVE_METAMAIL 1
-+#undef  PATH_METAMAIL
-+#define PATH_METAMAIL "metamail"
 +
  #endif /* !TIN_AUTOCONF_H */
